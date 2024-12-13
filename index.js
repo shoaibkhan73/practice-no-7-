5cc10088969ecf0b83e8e05ad5837f788dcce582
shoaibkhan73 <khan973973@gmail.com>
2024-12-13 20:08:01 +0500

    practice no 7 is comleted

---
+++ index.js
@@ -0,0 +1,37 @@
+console.log("practice no 7");
+
+// primitive data 
+
+// there are 7 typeof primitive data
+
+
+let a = "shoaib" ;
+let b = 414 ;
+let c = true ;
+let d = null ;
+let e ;
+let f = 545445487754247427n ;
+let g = Symbol(`442`) ;
+
+console.log(a , b , c , d , e , f , g);
+
+console.log(typeof a, typeof b , typeof c , typeof d , typeof e , typeof f , typeof g);
+
+
+// non primitive data type
+
+// Array  , Object  , function
+
+const names = [ " ali " , " shoaib " , " jawad " , " amir "];
+
+let myobj = {
+    name : "shoaib" ,
+    class: "software" ,
+}
+
+ let myfunction = function () {
+    console.log("my name is shoaib");
+}
+
+
+console.log(typeof names , typeof myobj , typeof myfunction);
